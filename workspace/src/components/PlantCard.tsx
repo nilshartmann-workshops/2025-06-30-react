8bccbd9bb074c3667c27203f46fd694e6c6954f3
Nils Hartmann <nils@nilshartmann.net>
2025-06-30 10:19:51 +0200

    Übung 01: React Komponente

---
+++ workspace/src/components/PlantCard.tsx
@@ -0,0 +1,25 @@
+// Komponentennamen mit Großbuchstaben am Anfang!
+
+export default function PlantCard() {
+  // Logik
+  // Daten
+  // View
+  // JSX
+  return <div>
+    <header>
+      <h2>Aloe Vera</h2>
+      <div>Schlafzimmer</div>
+    </header>
+    <section>
+      <div>Alle fünf Tage wässern</div>
+      <div>Zuletzt: 30.06.2025</div>
+    </section>
+  </div>
+}
+
+// export function Abc() {}
+export let person = "...";
+
+// export {
+//   PlantCard,
+// }
